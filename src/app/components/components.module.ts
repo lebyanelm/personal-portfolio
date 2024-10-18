32755de04088fa6add833f5c1dee066fc58c0be7
Libby Lebyane <lebyane.lm@gmail.com>
2024-10-18 23:30:07 +0200

    Added header

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HeroComponent],
-  exports: [HeroComponent],
+  imports: [CommonModule, HeroComponent, HeaderComponent],
+  exports: [HeroComponent, HeaderComponent],
 })
 export class ComponentsModule {}
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import { ScrollSmoother } from 'gsap/all';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-hero',
@@ -9,6 +10,45 @@ import { ScrollTrigger } from 'gsap/all';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {
-  @ViewChild('HeroContainer') heroContainer!: ElementRef<HTMLDivElement>;
+export class HeroComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.defaults({ ease: 'ease.inOut' });
+
+    // Blur fading effect
+    gsap.to('.hero-container', {
+      y: 100,
+      filter: 'blur(20px)',
+      scrollTrigger: {
+        trigger: '#main-content',
+        start: 'top bottom',
+        end: 'top center',
+        scrub: true,
+      },
+    });
+
+    // Remove the scroll indicator
+    gsap.to('.scroll-animation', {
+      y: 100,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: '#main-content',
+        start: 'top bottom',
+        end: 'top center',
+        scrub: true,
+      },
+    });
+
+    // Animate the plaques also
+    gsap.to('.plaque', {
+      y: 100,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: '#main-content',
+        start: 'top bottom',
+        end: 'top center',
+        scrub: true,
+      },
+    });
+  }
 }
